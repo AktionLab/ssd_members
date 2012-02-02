c5c3f5103c6ee6646d7e63d1d4ffec35edf91d71
Chris Boertien <chris.boertien@bz-labs.com>
2012-02-02 12:33:56 -0700

    add password fields to new admin user form
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -21,6 +21,8 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs "Member Details" do
       f.input :email
+      f.input :password
+      f.input :password_confirmation
     end
     f.buttons
   end
