e214a71a46ebaec8d3d2a824c80187a96a7e3015
Chris Boertien <chris.boertien@bz-labs.com>
2012-02-13 17:39:22 -0700

    setup test bootstrapping
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+  end
+end
+
+Spork.each_run do
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+end
+
