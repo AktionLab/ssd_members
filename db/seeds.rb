7d654f650882f8d7ca4b1bd0d49d5ca385aa4c9a
Chris Boertien <chris.boertien@bz-labs.com>
2012-02-15 00:11:15 -0700

    add seed data
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+models = %w(AdminUser Member Charge Payment).map(&:constantize)
+models.each(&:delete_all)
+
+AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+
+members = (1..3).map do |n|
+  Member.create!({
+    :name => "Test User #{n}",
+    :email => "test-#{n}@example.com",
+    :address => "123 45th St",
+    :phone => "555 555 5555",
+    :account_balance => n*100,
+    :account_category => Member::ACCOUNT_CATEGORIES.first
+  })
+end
+
+members.each do |member|
+  (1..3).each do |n|
+    member.payments.create(:amount => n*10)
+    member.charges.create(:amount => n*20)
+  end
+end
